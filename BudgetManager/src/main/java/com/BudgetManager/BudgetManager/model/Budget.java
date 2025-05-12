4edd835fa2dfce9050f1730ff60cb27a35c471cc
saidamst2003 <saida2003mst@gmail.com>
2025-05-13 00:11:02 +0100

    j'ai creer clas Tansaction

--- BudgetManager/src/main/java/com/BudgetManager/BudgetManager/model/Budget.java
+++ BudgetManager/src/main/java/com/BudgetManager/BudgetManager/model/Budget.java
@@ -14,6 +14,7 @@ import lombok.*;
 @RequiredArgsConstructor
 
 public class Budget {
+
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
--- BudgetManager/src/main/java/com/BudgetManager/BudgetManager/model/Transaction.java
+++ BudgetManager/src/main/java/com/BudgetManager/BudgetManager/model/Transaction.java
@@ -1,4 +1,44 @@
 package com.BudgetManager.BudgetManager.model;
+import jakarta.persistence.*;
 
+@Entity
 public class Transaction {
+    @Id
+    @GeneratedValue
+    private Long id;
+    private double amount;
+    private LocalDate date;
+    private String description;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public double getAmount() {
+        return amount;
+    }
+
+    public void setAmount(double amount) {
+        this.amount = amount;
+    }
+
+    public LocalDate getDate() {
+        return date;
+    }
+
+    public void setDate(LocalDate date) {
+        this.date = date;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
 }
