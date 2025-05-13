1d7be0ef4228d06be0c2f6f325912c4a46d8c5ea
lahcen404 <lahcen.maskour2003@gmail.com>
2025-05-13 12:10:39 +0100

    Create Budget Model

--- BudgetManager(BackEnd)/src/main/java/com/BudgetManager/BudgetManager/model/Budget.java
+++ BudgetManager(BackEnd)/src/main/java/com/BudgetManager/BudgetManager/model/Budget.java
@@ -3,6 +3,9 @@ package com.BudgetManager.BudgetManager.model;
 import jakarta.persistence.*;
 import lombok.*;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @Entity
 @Getter
 @Setter
@@ -16,9 +19,8 @@ public class Budget {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @ManyToOne(fetch = FetchType.LAZY)
-    @JoinColumn(name = "category_id", nullable = false)
-    private Category category;
+    @OneToMany(mappedBy = "budget",cascade = CascadeType.ALL)
+    private List<Category> category = new ArrayList<>();
 
     private double limitAmount ;
 
