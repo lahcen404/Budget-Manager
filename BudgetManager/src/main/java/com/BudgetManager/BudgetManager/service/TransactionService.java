939f80b7b772316c5424909eae45d3b1290e1ab4
saidamst2003 <saida2003mst@gmail.com>
2025-05-13 10:40:20 +0100

    j'ai creer l'ajoute

--- BudgetManager/src/main/java/com/BudgetManager/BudgetManager/service/TransactionService.java
+++ BudgetManager/src/main/java/com/BudgetManager/BudgetManager/service/TransactionService.java
@@ -1,4 +1,25 @@
 package com.BudgetManager.BudgetManager.service;
 
+import com.BudgetManager.BudgetManager.model.Transaction;
+import com.BudgetManager.BudgetManager.repository.TransactionRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.time.LocalDate;
+import java.util.Comparator;
+import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
+
+@Service
 public class TransactionService {
-}
+
+    @Autowired
+    private TransactionRepository transactionRepository;
+
+    // ➕ Ajouter une transaction
+    public Transaction addTransaction(Transaction transaction) {
+        return transactionRepository.save(transaction);
+    }
+
+
