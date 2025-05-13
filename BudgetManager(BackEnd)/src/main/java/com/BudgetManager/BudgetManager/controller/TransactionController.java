6aea1d6a69eb7b8dc1a54df9f50c81083d63fa59
saidamst2003 <saida2003mst@gmail.com>
2025-05-13 12:45:21 +0100

    j'ai creer llee

--- BudgetManager(BackEnd)/src/main/java/com/BudgetManager/BudgetManager/controller/TransactionController.java
+++ BudgetManager(BackEnd)/src/main/java/com/BudgetManager/BudgetManager/controller/TransactionController.java
@@ -1,4 +1,35 @@
 package com.BudgetManager.BudgetManager.controller;
 
+import com.BudgetManager.BudgetManager.model.Transaction;
+import com.BudgetManager.BudgetManager.service.TransactionService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.format.annotation.DateTimeFormat;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.time.LocalDate;
+import java.util.List;
+import java.util.Optional;
+
+@RestController
+@RequestMapping("/api/transactions")
+@CrossOrigin(origins = "*") // autoriser les requêtes CORS si besoin
 public class TransactionController {
+
+    @Autowired
+    private TransactionService transactionService;
+
+    // l'ajoute
+    @PostMapping
+    public ResponseEntity<Transaction> addTransaction(@RequestBody Transaction transaction) {
+        return ResponseEntity.ok(transactionService.addTransaction(transaction));
+    }
+
+    // Récupération
+    @GetMapping
+    public ResponseEntity<List<Transaction>> getAllTransactions() {
+        return ResponseEntity.ok(transactionService.getAllTransactions());
+    }
+
+
 }
