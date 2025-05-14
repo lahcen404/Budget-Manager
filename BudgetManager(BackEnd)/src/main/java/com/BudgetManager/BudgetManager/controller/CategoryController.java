682827330bd418a3fa574d41096f6e227ae401b0
lahcen404 <lahcen.maskour2003@gmail.com>
2025-05-14 11:27:37 +0100

    Create Functions CRUD Category

--- BudgetManager(BackEnd)/src/main/java/com/BudgetManager/BudgetManager/controller/CategoryController.java
+++ BudgetManager(BackEnd)/src/main/java/com/BudgetManager/BudgetManager/controller/CategoryController.java
@@ -2,13 +2,11 @@ package com.BudgetManager.BudgetManager.controller;
 
 import com.BudgetManager.BudgetManager.model.Budget;
 import com.BudgetManager.BudgetManager.model.Category;
-import com.BudgetManager.BudgetManager.repository.CategoryRepository;
 import com.BudgetManager.BudgetManager.service.CategoryService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/categories")
@@ -21,11 +19,28 @@ public class CategoryController {
     }
 
 
-
-
-
     @PostMapping
     public Category createCategory(@RequestBody Category category){
         return categoryService.createCategory(category);
+    }
+
+    @GetMapping
+    public List<Category> getAllCategories(){
+        return categoryService.getAllCategories();
+    }
+
+    @GetMapping("/{id}")
+    public Category getCategoryById(@PathVariable Long id){
+        return categoryService.getCategoryById(id).orElseThrow();
+    }
+
+    @PutMapping("/{id}")
+    public Category updateCategory(@PathVariable Long id, @RequestBody Category category) {
+        return categoryService.updateCategory(id,category);
+    }
+
+    @DeleteMapping("/{id}")
+    public void delete(@PathVariable Long id) {
+        categoryService.deleteCategory(id);
     }
 }
--- BudgetManager(BackEnd)/src/main/java/com/BudgetManager/BudgetManager/service/CategoryService.java
+++ BudgetManager(BackEnd)/src/main/java/com/BudgetManager/BudgetManager/service/CategoryService.java
@@ -1,9 +1,13 @@
 package com.BudgetManager.BudgetManager.service;
 
+import com.BudgetManager.BudgetManager.model.Budget;
 import com.BudgetManager.BudgetManager.model.Category;
 import com.BudgetManager.BudgetManager.repository.CategoryRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
 
 @Service
 public class CategoryService {
@@ -16,6 +20,34 @@ public class CategoryService {
 
     public Category createCategory(Category category){
         return categoryRepository.save(category);
+    }
+
+
+    public List<Category> getAllCategories() {
+        return categoryRepository.findAll();
+    }
+
+    public Optional<Category> getCategoryById(Long id) {
+        return categoryRepository.findById(id);
+    }
+
+
+    public void deleteCategory(Long id) {
+        categoryRepository.deleteById(id);
+    }
+
+
+
+
+    public Category updateCategory(Long id, Category categoryDetails) {
+        Category category = categoryRepository.findById(id).orElseThrow();
+        category.setName(categoryDetails.getName());
+        category.setLimitAmount(categoryDetails.getLimitAmount());
+        category.setTransactions(category.getTransactions());
+
+
+        return categoryRepository.save(category);
+
     }
 
 }
