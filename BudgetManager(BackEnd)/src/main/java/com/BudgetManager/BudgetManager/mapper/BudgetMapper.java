e869f3011049b7f50268f218519f3f28104a5a9c
lahcen404 <lahcen.maskour2003@gmail.com>
2025-05-15 10:44:01 +0100

    Create DTO for Validation

--- BudgetManager(BackEnd)/src/main/java/com/BudgetManager/BudgetManager/mapper/BudgetMapper.java
+++ BudgetManager(BackEnd)/src/main/java/com/BudgetManager/BudgetManager/mapper/BudgetMapper.java
@@ -1,4 +1,18 @@
 package com.BudgetManager.BudgetManager.mapper;
 
+import com.BudgetManager.BudgetManager.dto.BudgetDTO;
+import com.BudgetManager.BudgetManager.model.Budget;
+import com.BudgetManager.BudgetManager.model.Category;
+import org.springframework.stereotype.Component;
+
+@Component
 public class BudgetMapper {
+
+    public Budget toEntity(BudgetDTO dto, Category category){
+       Budget budget = new Budget();
+       budget.setLimitAmount(dto.getLimitAmount());
+       budget.setSpentAmount(dto.getSpentAmount());
+       budget.setCategory(category);
+        return budget;
+    }
 }
--- BudgetManager(BackEnd)/src/test/java/com/BudgetManager/BudgetManager/BudgetManagerApplicationTests.java
+++ BudgetManager(BackEnd)/src/test/java/com/BudgetManager/BudgetManager/BudgetManagerApplicationTests.java
@@ -1,13 +1,20 @@
 package com.BudgetManager.BudgetManager;
 
+import com.BudgetManager.BudgetManager.model.Budget;
+import com.BudgetManager.BudgetManager.model.Category;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 @SpringBootTest
 class BudgetManagerApplicationTests {
 
 	@Test
-	void contextLoads() {
+	void testCreateBudget() {
+
+
 	}
 
 }
